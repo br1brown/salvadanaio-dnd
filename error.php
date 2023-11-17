dd9faa70c2c082b36f2d857b636a3c24a11d928e
br1brown <Finestra-200>
2023-11-17 14:51:56 +0100

    messo header comune

--- error.php
+++ error.php
@@ -44,43 +44,8 @@ $codes = array(
 
 ?>
 <head>
-
-	<!-- Definizione della codifica dei caratteri per la pagina -->
-	<meta charset="UTF-8">
-
-	<!-- Impostazioni per il responsive design e il controllo della visualizzazione su dispositivi mobili -->
-	<meta name="viewport" content="width=device-width, initial-scale=1"> <!--shrink-to-fit=no-->
-
-	<!-- Informazioni sull'autore del sito -->
-	<meta name="author" content="Br1Brown">
-
 	<title>Errore</title>
-
-	<!-- ROBE PER IL MENU + SOCIAL anche quella prima per delle altre icone -->
-	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css">
-	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-
-	<!-- jquery -->
-	<script src="https://code.jquery.com/jquery-3.5.1.js"></script>
-	<script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
-
-	<!-- bootstrap -->
-	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
-	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" />
-
-	<!-- GLI ALERT -->
-	<script src="https://cdn.jsdelivr.net/npm/sweetalert2@10"></script>
-
-	<!-- Optional: include a polyfill for ES6 Promises for IE11 -->
-	<script src="https://cdn.jsdelivr.net/npm/promise-polyfill"></script>
-
-	<!-- ROBA NOSTRA -->
-	<link rel="stylesheet" href="style/base.css">
-	<link rel="stylesheet" href="style/manage_img.css">
-	<link rel="stylesheet" href="style/social.css">
-	<script src="script/base.js"></script>
-	<!-- SFONDO CON LE NUVOLE -->
-	<script src="script/jquery_bloodforge_smoke_effect.js"></script>
+	<?php include('header.php'); ?>
 </head>
 <body>
 	<canvas id="smoke-effect-canvas"
@@ -88,7 +53,7 @@ $codes = array(
 	<div class="container-fluid">
 		<div class="row">
 			<div class="col-12 col-md-2">
-				<a href="\" class="btn btn-warning btn-sm w-100">Home</a>
+				<a href="index" class="btn btn-warning btn-sm w-100">Home</a>
 			</div>
 			<div id=contenuto class="col-12 col-md-8 text-center tutto">
 				<?php
