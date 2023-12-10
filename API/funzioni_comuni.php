9ad0e94ba6fe1762491030b8d9d9103cb5ad3cb0
br1brown <Finestra-200>
2023-12-10 17:18:26 +0100

    Aggiunta politica CORS

--- API/funzioni_comuni.php
+++ API/funzioni_comuni.php
@@ -1,6 +1,24 @@
 <?php
+// Gestione CORS
+$fileconfigCORS = "CORSconfig.json";
+if (file_exists($fileconfigCORS)){
+    $config = json_decode(file_get_contents($fileconfigCORS), true);
+    $applyCORS = $config['applyCORS'];
+    $allowedOrigins = $config['allowedOrigins'] ?? [];
+    if ($applyCORS) {
+        if (empty($allowedOrigins)) {
+            header("Access-Control-Allow-Origin: *");
+        } elseif (in_array($origin, $allowedOrigins)) {
+            $origin = isset($_SERVER['HTTP_ORIGIN']) ? $_SERVER['HTTP_ORIGIN'] : '';
+            header("Access-Control-Allow-Origin: $origin");
+        }
+        header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
+        header("Access-Control-Allow-Headers: Content-Type, Authorization");
+    }
+}
 require_once __DIR__.'/BLL/Repository.php';
 require_once __DIR__.'/BLL/Response.php';
+
 
 /**
  * Ottiene un oggetto e lo restituisce, eventualmente dopo aver applicato una callback.
