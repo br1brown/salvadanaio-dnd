1ce12494c9d0f74cb3bff1936de38a5410666056
br1brown <Finestra-200>
2024-01-14 12:27:24 +0100

    Chiarezza nel front end

--- script/addon.js
+++ script/addon.js
@@ -1,4 +1,24 @@
-//https://sweetalert2.github.io/
-$(document).ready(function () {
+// Questo script utilizza la libreria SweetAlert2 per mostrare avvisi personalizzati.
+// La documentazione della libreria è disponibile su: https://sweetalert2.github.io/
 
+// Attende che il DOM sia completamente caricato
+$(document).ready(function () {
+    // Gestisce il click su elementi con classe 'bottone'
+    $(".bottone").click(function () {
+        // Rimuove il focus dal bottone dopo il click
+        $(this).blur();
+
+        // Recupera il valore (val) e il tipo (data-type) del bottone cliccato
+        var val = $(this).val();
+        var tipo = $(this).data("type");
+
+        // Controlla se il 'tipo' è definito e non vuoto
+        if (tipo && tipo != "") {
+            // Visualizza un SweetAlert con il tipo e il valore del bottone
+            swal.fire(tipo + ": Sono io", val, tipo);
+        } else {
+            // Se 'tipo' non è definito, visualizza un SweetAlert con un messaggio predefinito
+            swal.fire("Sono io di Br1Brown", val);
+        }
+    });
 });
--- script/base.js
+++ script/base.js
@@ -1,80 +1,73 @@
-//https://sweetalert2.github.io/
+// Attende che il DOM sia completamente caricato
 $(document).ready(function () {
-	$(".bottone").click(function () {
-		$(this).blur();
-		var val = $(this).val()
-		var tipo = $(this).data("type");
-		if (tipo && tipo != "")
-			swal.fire(tipo + ": Sono io", val, tipo);
-		else
-			swal.fire("Sono io di Br1Brown", val);
-	});
-
-
+	// Gestisce l'evento di scorrimento della finestra
 	$(window).scroll(function () {
+		// Controlla se lo scroll supera i 50 pixel
 		if ($(this).scrollTop() > 50) {
-			$('#back-to-top').fadeIn();
+			$('#back-to-top').fadeIn(); // Mostra il pulsante 'back-to-top'
 		} else {
-			$('#back-to-top').fadeOut();
+			$('#back-to-top').fadeOut(); // Nasconde il pulsante 'back-to-top'
 		}
 	});
 
-
-	// scroll body to 0px on click
+	// Gestisce il click sul pulsante 'back-to-top'
 	$('#back-to-top').click(function () {
+		// Anima lo scroll verso l'alto della pagina
 		$('body,html').animate({
 			scrollTop: 0
-		}, 400);
+		}, 400); // 400 millisecondi per l'animazione
 		return false;
 	});
 
+	// Imposta l'effetto fumo su un elemento canvas
 	var fumo = $('#smoke-effect-canvas');
 	fumo.SmokeEffect({
-		color: fumo.data('color'),
-		opacity: fumo.data('opacity'),
-		maximumVelocity: fumo.data('maximumVelocity'),
-		particleRadius: fumo.data('particleRadius'),
-		density: fumo.data('density')
+		color: fumo.data('color'), // Colore del fumo
+		opacity: fumo.data('opacity'), // Opacità del fumo
+		maximumVelocity: fumo.data('maximumVelocity'), // Velocità massima delle particelle
+		particleRadius: fumo.data('particleRadius'), // Raggio delle particelle di fumo
+		density: fumo.data('density') // Densità del fumo
 	});
-
-
 });
 
-
+// Funzione per aprire un link codificato
 function openEncodedLink(prefix, encodedStr) {
-	//var parser = new DOMParser();
-	// var decodedString = parser.parseFromString("&#" + encodedStr + ";", "text/html").documentElement.textContent;
-	var decodedString = encodedStr; //pare che il browser faccia lui le cose
-
+	var decodedString = encodedStr; // Decodifica la stringa (assumendo che il browser gestisca l'encoding)
 	var url = "";
+
+	// Costruisce l'URL completo
 	if (prefix) {
 		url = prefix + decodedString;
 	} else {
 		url = decodedString;
 	}
+
+	// Reindirizza alla nuova URL
 	window.location.href = url;
 }
 
+// Funzione per impostare lo sfondo con il colore medio di un'immagine
 function set_background_with_average_rgb(src) {
 	var canvas = document.createElement('canvas');
 	var context = canvas.getContext('2d');
 	var img = new Image();
 
-	// Setto crossOrigin per evitare problemi di CORS su immagini da domini esterni.
+	// Imposta crossOrigin per evitare problemi di CORS con immagini esterne
 	img.crossOrigin = 'Anonymous';
 
 	img.onload = function () {
-		// Disegno l'immagine nel canvas. La dimensione 1x1 è sufficiente per il calcolo medio del colore.
+		// Disegna l'immagine nel canvas a dimensioni ridotte
 		context.drawImage(img, 0, 0, 1, 1);
 		var data = context.getImageData(0, 0, 1, 1).data;
 
-		// Calcolo il colore medio e lo imposto come sfondo della pagina.
+		// Calcola il colore medio e lo imposta come sfondo del body
 		var colorStr = 'rgb(' + data[0] + ',' + data[1] + ',' + data[2] + ')';
 		document.body.style.backgroundColor = colorStr;
 	};
 
 	img.onerror = function () {
+		// Gestione dell'errore di caricamento immagine
 	};
 
-	img.src = src;
+	img.src = src; // Imposta la sorgente dell'immagine
 }
