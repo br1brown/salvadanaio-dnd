79235a78c050eddfdea36d0c539a36bcb9ff2f90
br1brown <br1brown@hotmail.it>
2024-02-11 06:00:28 +0100

    raccolte eccezioni per gestione metodi

--- API/BLL/gestione_metodi.php
+++ API/BLL/gestione_metodi.php
@@ -1,16 +1,13 @@
 <?php
-$metodo = $_SERVER['REQUEST_METHOD'];
 
-$gestoriMetodi = [
-    'GET' => 'eseguiGET',
-    'POST' => 'eseguiPOST',
-    'PUT' => 'eseguiPUT',
-    'DELETE' => 'eseguiDELETE'
-];
-
+$funzione = "esegui".$_SERVER['REQUEST_METHOD'];
 // Verifica se il metodo è gestito e se la funzione corrispondente esiste
-if (isset($gestoriMetodi[$metodo]) && function_exists($gestoriMetodi[$metodo])) {
-    call_user_func($gestoriMetodi[$metodo]); // Chiama la funzione corrispondente
+if (isset($funzione) && function_exists($funzione)) {
+    try{
+        call_user_func($funzione); // Chiama la funzione corrispondente
+    } catch (Exception $e) {
+        echo BLL\Response::retError($e->getMessage());
+    } 
 } else {
     header("HTTP/1.1 405 Method Not Allowed");
 }
