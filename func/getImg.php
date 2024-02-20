89da7ba0d2793da1ebd00c9b9b2498d4af9315f9
br1brown <br1brown@hotmail.it>
2024-02-20 11:35:05 +0100

    migliorato recupero immagini

--- func/getImg.php
+++ func/getImg.php
@@ -5,19 +5,27 @@ require_once(dirname(__DIR__) . "/FE_utils/Asset.php");
 $imgID = isset($_GET['imgID']) ? $_GET['imgID'] : '';
 
 // Usa la classe Asset per ottenere il percorso dell'immagine
-$file_path = dirname(__DIR__) . "/asset/" . Asset::getPath($imgID);
+$path = Asset::getPath($imgID);
 
-if ($file_path !== null && file_exists($file_path)) {
-    // Determina il MIME type del file
-    $content_type = Asset::getMimeType($file_path);
-
-    // Imposta l'header corretto per il tipo di file
-    header("Content-Type: $content_type");
-    // Leggi e restituisce il file
-    readfile($file_path);
+// Verifica se $path è un URL
+if (filter_var($path, FILTER_VALIDATE_URL)) {
+    // Reindirizza al browser per visualizzare l'immagine dall'URL
+    header("Location: $path");
+    exit;
 } else {
-    // Restituisci un errore se il file non esiste o l'ID non è valido
-    http_response_code(404);
-    echo "Immagine non trovata o ID immagine non valido";
+    // Costruisce il percorso del file locale
+    $file_path = dirname(__DIR__) . "/asset/" . $path;
+
+    if ($path !== null && file_exists($file_path)) {
+        // Determina il MIME type del file
+        $content_type = Asset::getMimeType($file_path);
+
+        // Imposta l'header corretto per il tipo di file
+        header("Content-Type: $content_type");
+        // Leggi e restituisce il file
+        readfile($file_path);
+    } else {
+        // Restituisci un errore se il file non esiste o l'ID non è valido
+        http_response_code(404);
+    }
 }
-?>
