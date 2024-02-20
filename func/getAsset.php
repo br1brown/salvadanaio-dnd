dadd103978d5d05c4980713edf8dc9bf08a49b6c
br1brown <br1brown@hotmail.it>
2024-02-20 18:23:01 +0100

    resize immagini troppo grandi per non appesantire il sito

--- func/getAsset.php
+++ func/getAsset.php
@@ -1,31 +1,88 @@
 <?php
 require_once(dirname(__DIR__) . "/FE_utils/Asset.php");
 
-// Prendi l'imgID dal parametro GET
-$imgID = isset($_GET['ID']) ? $_GET['ID'] : '';
+function LoadSmallImage($file_path, $content_type)
+{
+    list($width, $height) = getimagesize($file_path);
+    if (!$width || !$height) {
+        // Gestire l'errore, ad esempio loggando l'errore e restituendo un'immagine di fallback o un messaggio di errore
+        http_response_code(400);
+        exit;
+    }
+    // Dimensioni massime HD
+    $max_width = 1280;
+    $max_height = 720;
 
-// Usa la classe Asset per ottenere il percorso dell'immagine
-$path = Asset::getPath($imgID);
+    if ($width > $max_width || $height > $max_height) {
+        // Calcola il rapporto di ridimensionamento
+        $scale = min($max_width / $width, $max_height / $height);
 
-// Verifica se $path è un URL
+        $new_width = ceil($scale * $width);
+        $new_height = ceil($scale * $height);
+
+        // Crea una nuova immagine ridimensionata
+        $image_resized = imagecreatetruecolor($new_width, $new_height);
+        $image_original = imagecreatefromstring(file_get_contents($file_path));
+
+        // Copia e ridimensiona l'immagine vecchia nella nuova
+        imagecopyresampled($image_resized, $image_original, 0, 0, 0, 0, $new_width, $new_height, $width, $height);
+
+        // Output dell'immagine ridimensionata
+        header("Content-Type: $content_type");
+        switch ($content_type) {
+            case 'image/jpeg':
+                imagejpeg($image_resized);
+                break;
+            case 'image/png':
+                imagepng($image_resized);
+                break;
+            case 'image/gif':
+                imagegif($image_resized);
+                break;
+            default:
+                // Gestire il tipo di immagine non supportato
+                http_response_code(415);
+                exit;
+        }
+
+        // Pulizia della memoria
+        imagedestroy($image_resized);
+        imagedestroy($image_original);
+    } else {
+        // Se l'immagine è già entro i limiti, mostra come è
+        header("Content-Type: $content_type");
+        readfile($file_path);
+    }
+}
+
+$sanitizedID = preg_replace('/[^a-zA-Z0-9_-]/', '', isset($_GET['ID']) ? $_GET['ID'] : '');
+
+// Verifica che l'ID sanificato corrisponda a un formato atteso, ad esempio un numero o un identificativo alfanumerico
+if (!preg_match('/^[a-zA-Z0-9_-]+$/', $sanitizedID)) {
+    http_response_code(400); // Bad Request
+    exit;
+}
+
+// Continua con il processo di caricamento dell'immagine usando l'ID sanificato
+$path = Asset::getPath($sanitizedID);
+
 if (filter_var($path, FILTER_VALIDATE_URL)) {
-    // Reindirizza al browser per visualizzare l'immagine dall'URL
     header("Location: $path");
     exit;
 } else {
-    // Costruisce il percorso del file locale
     $file_path = dirname(__DIR__) . "/asset/" . $path;
 
     if ($path !== null && file_exists($file_path)) {
-        // Determina il MIME type del file
         $content_type = Asset::getMimeType($file_path);
 
-        // Imposta l'header corretto per il tipo di file
-        header("Content-Type: $content_type");
-        // Leggi e restituisce il file
-        readfile($file_path);
+        // Verifica se il content type è un'immagine prima di procedere
+        if (strpos($content_type, 'image/') === 0) {
+            LoadSmallImage($file_path, $content_type);
+        } else {
+            header("Content-Type: $content_type");
+            readfile($file_path);
+        }
     } else {
-        // Restituisci un errore se il file non esiste o l'ID non è valido
         http_response_code(404);
     }
 }
